6c38a58d0bffad568e9e24fcea895c3883012a50
Anna Boval <anna.bo1106@gmail.com>
2022-04-14 15:07:59 +0200

    axios done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import './App.css';
-import axios from 'axios;';
+import axios from 'axios';
+import DisplayCharacter from './components/DisplayCharacter';
 import React, { useEffect, useState } from 'react';
 
 export default function App() {
   const [character, setCharacter] = useState();
   React.useEffect(() => {
-  axios 
-  .get("https://simpsons-quotes-api.herokuapp.com/quotes")
-  .then((answer) => answer.data)
-  .then((data) => { setCharacter(data[0]);
-   });
+    axios
+      .get('https://simpsons-quotes-api.herokuapp.com/quotes')
+      .then((answer) => answer.data)
+      .then((data) => {
+        setCharacter(data[0]);
+      });
   }, []);
-  return (<>
-   <DisplayCharacter character={Character} /></>
+  return (
+    <div className='App'>
+      <DisplayCharacter character={character} />
+    </div>
+  );
+}
--- src/components/DisplayCharacter.js
+++ src/components/DisplayCharacter.js
@@ -1,5 +1,13 @@
-const DisplayCharacter = () => {
-  return <div></div>;
+const DisplayCharacter = ({ character }) => {
+  return (
+    character && (
+      <>
+        <h1>{character.character}</h1>
+        <p>{character.quote}</p>
+        <img src={character.image} alt={character.character}></img>
+      </>
+    )
+  );
 };
 
 export default DisplayCharacter;
