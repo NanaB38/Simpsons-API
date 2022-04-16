3061e637c8f7e68f890c74852be7939a7f9d799b
Anna Boval <anna.bo1106@gmail.com>
2022-04-16 18:12:10 +0200

    API request ok quest done

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import './App.css';
 import axios from 'axios';
 import DisplayCharacter from './components/DisplayCharacter';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 
 export default function App() {
-  const [character, setCharacter] = useState();
-  React.useEffect(() => {
+  const [character, setCharacter] = useState('');
+
+  const getCharacter = () => {
     axios
       .get('https://simpsons-quotes-api.herokuapp.com/quotes')
       .then((answer) => answer.data)
       .then((data) => {
         setCharacter(data[0]);
       });
-  }, []);
+  };
   return (
     <div className='App'>
       <DisplayCharacter character={character} />
+      <button type='button' onClick={getCharacter}>
+        Get a random Character !
+      </button>
     </div>
   );
 }
--- src/components/DisplayCharacter.js
+++ src/components/DisplayCharacter.js
@@ -1,11 +1,11 @@
 const DisplayCharacter = ({ character }) => {
   return (
     character && (
-      <>
+      <div className='DisplayCharacter'>
         <h1>{character.character}</h1>
         <p>{character.quote}</p>
         <img src={character.image} alt={character.character}></img>
-      </>
+      </div>
     )
   );
 };
